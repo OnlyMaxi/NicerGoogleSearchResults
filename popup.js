78de794cd0677c600f3967b2ddfd0ee2cd6dcad1
Only_Maxi <only.maxi.milian@gmail.com>
2025-09-03 17:09:36 +0200

    Changing preferences now creates an "apply changes" button that reloads the page when clicked to apply the new preferences

--- popup.js
+++ popup.js
@@ -1,3 +1,38 @@
+function createApply() {
+    if (document.querySelector("#applyButton")) return; //button already exists
+
+    const apply = document.createElement("div");
+    const applyButton = document.createElement("button");
+    apply.id = "apply";
+    applyButton.id = "applyButton";
+
+    applyButton.textContent = "Apply changes (will reload page!)";
+    applyButton.addEventListener("click", applyChanges);
+
+    apply.appendChild(applyButton);
+    document.body.appendChild(apply);
+}
+
+function applyChanges() {
+    const apply = document.querySelector("#apply");
+    if (apply) apply.innerHTML = "";
+
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        if (tabs[0].id) {
+            chrome.tabs.reload(tabs[0].id);
+        }
+    });
+}
+
+function updatePreference(key, value) {
+    chrome.storage.sync.get(["preferences"], (data) => {
+        const preferences = data.preferences || {};
+        preferences[key] = value;
+        chrome.storage.sync.set({preferences});
+        createApply();
+    })
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const elements = {
         removeAIO: document.querySelector("#removeAIOverview"),
@@ -9,14 +44,6 @@ document.addEventListener("DOMContentLoaded", () => {
         elements.removeAIO.checked = preferences.removeAIO ?? false;
         elements.removePAA.checked = preferences.removePAA ?? false;
     });
-
-    function updatePreference(key, value) {
-        chrome.storage.sync.get(["preferences"], (data) => {
-            const preferences = data.preferences || {};
-            preferences[key] = value;
-            chrome.storage.sync.set({preferences});
-        })
-    }
 
     elements.removeAIO.addEventListener('change', (e) => {
         console.log("remove ai overview: " + elements.removeAIO.checked);
