710eadfadafa013a20544619da7bad36aa8f7122
Only_Maxi <only.maxi.milian@gmail.com>
2025-09-06 19:15:35 +0200

    code cleanup

--- content.js
+++ content.js
@@ -8,14 +8,12 @@ chrome.storage.sync.get("preferences", data => {
         document.querySelectorAll("h1").forEach(result => {
             if (result.innerHTML !== "AI overview") return;
 
-            result.parentElement.style.border = "5px dashed lime"; //testing
             result.parentElement.remove();
         });
         //sometimes and on smaller screens, the heading is a h2 instead of a h1
         document.querySelectorAll("h2").forEach(result => {
             if (result.innerHTML !== "AI overview") return;
 
-            result.parentElement.style.border = "5px dashed lime"; //testing
             result.parentElement.remove();
         });
     }
@@ -26,7 +24,6 @@ chrome.storage.sync.get("preferences", data => {
             if (result.innerHTML !== "People also ask" && result.innerHTML !== "People also search for") return;
 
             const parent = result.parentElement.parentElement.parentElement.parentElement; //4 steps up should always be safe
-            //parent.style.border = "5px dashed lime"; //testing
             parent.remove();
         });
     }
